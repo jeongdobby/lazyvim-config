16ea7031421be104ab3a0a8af8c30068e9b6d4ba
dobby <jmjeong@uniquify.com>
2025-01-23 11:00:42 +0900

    delete cscope option

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,27 +2,4 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
--- cscope setting
-vim.opt.tags = { './tags', 'tags', '../tags', '../../tags', '../../../tags', '../../../../tags' }
-vim.opt.csprg = '/usr/bin/cscope'
-vim.opt.csto = 0
-vim.opt.cst = true
-vim.opt.nocsverb = true
-
-if vim.fn.filereadable('./cscope.out') == 1 then
-    vim.cmd('cs add ./cscope.out')
-elseif vim.fn.filereadable('../cscope.out') == 1 then
-    vim.cmd('cs add ../cscope.out')
-elseif vim.fn.filereadable('../../cscope.out') == 1 then
-    vim.cmd('cs add ../../cscope.out')
-elseif vim.fn.filereadable('../../../cscope.out') == 1 then
-    vim.cmd('cs add ../../../cscope.out')
-elseif vim.fn.filereadable('../../../../cscope.out') == 1 then
-    vim.cmd('cs add ../../../../cscope.out')
-else
-    vim.cmd('cs add /usr/src/linux/cscope.out')
-end
-
-vim.opt.cscopeverbose = true
-
 vim.opt.clipboard = vim.env.SSH_TTY and "unnamedplus" or "unnamedplus"
