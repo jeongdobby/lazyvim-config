043696bde547884202950f95899c7ad6dafbc714
dobby <jjm4468@naver.com>
2025-02-10 11:15:40 +0900

    sync up

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -4,4 +4,6 @@
 
 vim.opt.clipboard = vim.env.SSH_TTY and "unnamedplus" or "unnamedplus"
 vim.opt.relativenumber = false -- Relative line numbers
-vim.opt.shiftwidth = 8 -- Size of an indent
+vim.opt.shiftwidth = 4 -- Size of an indent
+vim.opt.tabstop = 4 -- Size of an indent
+vim.opt.expandtab = false -- Use spaces instead of tabs
